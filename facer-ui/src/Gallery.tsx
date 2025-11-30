bf4ceba8408d83a18330d7fc277d1223edf9a645
saltchicken <john.eicher89@gmail.com>
2025-11-30 12:07:00 -0800

    Display is_valid_pose instead of direction

--- facer-ui/src/Gallery.tsx
+++ facer-ui/src/Gallery.tsx
@@ -359,7 +359,8 @@ export default function Gallery() {
                   ? 'bg-green-500/20 text-green-400 border-green-500/30'
                   : 'bg-red-500/20 text-red-400 border-red-500/30'
                   }`}>
-                  {face.direction}
+
+                  {face.is_valid_pose ? "Valid" : "Invalid"}
                 </span>
               </div>
             </div>
